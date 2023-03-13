8d1fa0a097510a0c44ba9815414f3442b41e00b2
DbIXAHUEOKEAHA <mishanya319@gmail.com>
2023-03-13 18:06:51 +0800

    Keithley2400 sweepable

--- devices/Keithley2400.py
+++ devices/Keithley2400.py
@@ -1,4 +1,6 @@
 import pyvisa as visa
+import numpy as np
+import time
 
 rm = visa.ResourceManager()
 
@@ -7,7 +9,7 @@ def get(device, command):
     # return np.round(np.random.random(1), 1)
     return device.query(command)
 
-class keithley2400():
+class Keithley2400():
     
     def __init__(self, adress = 'GPIB0::7::INSTR'):
         self.sm = rm.open_resource(
@@ -15,28 +17,39 @@ class keithley2400():
         
         self.sm.write(':SOUR:CLE:AUTO OFF') # auto on/off
         
-        self.set_options = ['V', 'I', 'compl_curr', 'V_NPLC', 'I_NPLC', 'R_NPLC']
+        self.set_options = ['Volt', 'Curr', 'compl_curr', 'V_NPLC', 'I_NPLC', 'R_NPLC']
         
-        self.get_options = ['V', 'I', 'R', 'compl_curr', 'V_NPLC', 'I_NPLC', 'R_NPLC', 'line_freq', 'Sdelay', 'TDelay']
+        self.get_options = ['Volt', 'Curr', 'Res', 'compl_curr', 'V_NPLC', 'I_NPLC', 'R_NPLC', 'line_freq', 'Sdelay', 'TDelay']
+        
+        self.sweepable = [True, True, False, False, False, False]
+        
+        self.maxspeed = [0.5, 0.1, None, None, None, None]
         
     def IDN(self):
         return get(self.sm, '*IDN?')
     
-    def V(self):
+    
+    def Volt(self):
         try:
             answer = get(self.sm, ':MEAS:VOLT?').split(',')[0]
         except ValueError:
             answer = get(self.sm, ':MEAS:VOLT?').split(',')[0]
+            
+        self.cur_volt = answer
+            
         return float(answer)
         
-    def I(self):
+    def Curr(self):
         try:
             answer = get(self.sm, ':MEAS:CURR?').split(',')[1]
         except ValueError:
             answer = get(self.sm, ':MEAS:CURR?').split(',')[1]
+            
+        self.cur_curr = answer
+            
         return float(answer)
         
-    def R(self):
+    def Res(self):
         try:
             answer = get(self.sm, ':MEAS:RES?').split(',')[2]
         except ValueError:
@@ -73,13 +86,17 @@ class keithley2400():
         self.sm.write(f':SENS:CURR:PROT {str(round(float(value), 5))}')
         
     def V_NPLC(self):
-        return float(get(self.sm, ':SENS:VOLT:NPLC?'))
+        answer = float(get(self.sm, ':SENS:VOLT:NPLC?'))
+        self.vnplc = answer
+        return answer
         
     def set_V_NPLC(self, value = 1):
         self.sm.write(':SENS:VOLT:NPLC ' + str(round(float(value), 2)))
         
     def I_NPLC(self):
-        return float(get(self.sm, ':SENS:CURR:NPLC?'))
+        answer = float(get(self.sm, ':SENS:CURR:NPLC?'))
+        self.inplc = answer
+        return answer
         
     def set_I_NPLC(self, value = 1):
         self.sm.write(':SENS:CURR:NPLC ' + str(round(float(value), 2)))
@@ -95,13 +112,89 @@ class keithley2400():
         return answer
         
     def line_freq(self):
-        return float(get(self.sm, ':SYST:LFR?'))
+        answer = float(get(self.sm, ':SYST:LFR?'))
+        self.freq = answer
+        return answer
     
     def Sdelay(self):
-        return float(get(self.sm, ':SOUR:DEL?'))
+        answer = float(get(self.sm, ':SOUR:DEL?'))
+        self.sdelay = answer
+        return answer
     
     def Tdelay(self):
-        return float(get(self.sm, ':TRIG:DEL?'))
+        answer = float(get(self.sm, ':TRIG:DEL?')) 
+        self.tdelay = float(get(self.sm, ':TRIG:DEL?'))
+        return answer
+    
+    def set_Volt(self, value, speed):
+        """ Ramps to a target voltage from the set voltage value over
+        a certain number of linear steps, each separated by a pause duration.
+        """
+        
+        self.Volt()
+        
+        if not hasattr(self, 'freq'):
+            self.line_freq()
+            
+        if not hasattr(self, 'sdelay'):
+            self.Sdelay()
+            
+        if not hasattr(self, 'tdelay'):
+            self.Tdelay()
+            
+        if not hasattr(self, 'vnplc'):
+            self.V_NPLC()
+            
+        dv = float(value) - self.cur_volt
+        nsteps = dv / (speed * (self.sdelay + self.vnplc / self.freq + self.tdelay))
+        nsteps = int(round(nsteps))
+        
+        dt = dv / speed
+        dt = dt / nsteps
+        
+        voltages = np.linspace(
+            self.cur_volt,
+            value,
+            nsteps
+        )
+        for voltage in voltages:
+            self.set_V(voltage)
+            time.sleep(dt)
+            
+    def set_Curr(self, value, speed):
+        """ Ramps to a target current from the set current value over
+        a certain number of linear steps, each separated by a pause duration.
+        """
+        
+        self.Curr()
+        
+        if not hasattr(self, 'freq'):
+            self.line_freq()
+            
+        if not hasattr(self, 'sdelay'):
+            self.Sdelay()
+            
+        if not hasattr(self, 'tdelay'):
+            self.Tdelay()
+            
+        if not hasattr(self, 'inplc'):
+            self.I_NPLC()
+            
+        di = float(value) - self.cur_curr
+        nsteps = di / (speed * (self.sdelay + self.vnplc / self.freq + self.tdelay))
+        nsteps = int(round(nsteps))
+        
+        dt = di / speed
+        dt = dt / nsteps
+        
+        currents = np.linspace(
+            self.cur_curr,
+            value,
+            nsteps
+        )
+        for current in currents:
+            self.set_I(current)
+            time.sleep(dt)
     
 def main():
     device = Keithley2400()
