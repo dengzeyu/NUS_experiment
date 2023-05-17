34143f37b7119ca34a7032f98aa18ca74760aba2
Mikhail Kravtsov <e0974196@nus.edu.sg>
2023-05-18 02:34:53 +0800

    Timeout added, sys.exit removed

--- devices/opticool.py
+++ devices/opticool.py
@@ -1,12 +1,128 @@
 from MultiPyVu import MultiVuClient as mvc
 from enum import IntEnum
+import socket
+from MultiPyVu.SocketMessageClient import ClientMessage
+from MultiPyVu.instrument import MultiVuExeException
+from typing import Dict
+import time
+import sys
+if sys.platform == 'win32':
+    try:
+        import msvcrt    # Used to help detect the esc-key
+    except ImportError:
+        print("Must import the pywin32 module.  Use:  ")
+        print("\tconda install -c conda-forge pywin32")
+        print("   or")
+        print("\tpip install pywin32")
+        sys.exit(0)
+
+class client(mvc.MultiVuClient):
+    
+    def __enter__(self):
+        self.logger.info(f'Starting connection to {self._addr}')
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setblocking(False)
+        self.sock.connect_ex(self._addr)
+
+        self._message = ClientMessage(self._sel,
+                                      self.sock,
+                                      )
+        # send a request to the sever to confirm a connection
+        max_tries = 1
+        action = 'START'
+        for attempt in range(max_tries):
+            try:
+                self.response = self._send_and_receive(action)
+            except OSError as e:
+                msg = f'Attempt {attempt + 1} of {max_tries} failed:  {e}'
+                self.logger.info(msg)
+                time.sleep(1)
+                if attempt == max_tries - 1:
+                    err_msg = 'Failed to make a connection to the '
+                    err_msg += 'server.  Check if the MultiVuServer '
+                    err_msg += 'is running.'
+                    self.logger.info(err_msg)
+            else:
+                self.logger.info(self.response['result'])
+                break
+
+        return self
+    
+    def _monitor_and_get_response(self) -> Dict[str, str]:
+        '''
+        This monitors the traffic going on.  It asks the SocketMessageClient
+        class for help in understanding the data.  This is also used to handle
+        the possible errors that SocketMessageClient could generate.
+
+        Raises
+        ------
+        ConnectionRefusedError
+            Could be raised if the server is not running.
+        ConnectionError
+            Could be raised if there are connection issues with the server.
+        KeyboardInterrupt
+            This is used by the user to close the connection.
+        MultiVuExeException
+            Raised if there are issues with the request for MultiVu commands.
+
+        Returns
+        -------
+        TYPE
+            The information retrived from the socket and interpreted by
+            SocketMessageClient class.
+
+        '''
+        while True:
+            events = self._sel.select(timeout=self._socketIoTimeout)
+            if events:
+                for key, mask in events:
+                    message = key.data
+                    try:
+                        message.process_events(mask)
+                    except ConnectionAbortedError as e:
+                        # Client closed the server
+                        raise ConnectionAbortedError from e
+                    except ConnectionError as e:
+                        # Client closed the client
+                        raise ConnectionError from e
+                    except Exception:
+                        self._close_and_exit()
+                    else:
+                        # Windows looks for the ESC key to quit.
+                        if sys.platform == 'win32':
+                            escKey = chr(27)
+                            if (msvcrt.kbhit()
+                                    and msvcrt.getch().decode() == escKey):
+                                self._thread_running = False
+                                raise KeyboardInterrupt
+                        # return the response
+                        self.response = message.response
+                        if (self.response is not None
+                                and message.request is None):
+                            rslt = self.response['result']
+                            if rslt.startswith('MultiVuError:'):
+                                self.logger.info(self.response['result'])
+                                message.close()
+                                raise MultiVuExeException(rslt)
+                            else:
+                                return self.response
+            else:
+                # An empty list means the selector timed out
+                msg = 'Socket timed out after '
+                msg += f'{self._socketIoTimeout} seconds.'
+                raise TimeoutError(msg)
+            # Check for a socket being monitored to continue.
+            if not self._sel.get_map():
+                break
 
 class opticool():
-    def __init__(self, adress = '192.168.1.207:5000'):
+    def __init__(self, adress = '192.168.0.181:5000'):
         host, port = adress.split(':')
         port = int(port)
         
-        self.device = mvc.MultiVuClient(host=host, port = port)
+        timeout = 2 #s
+        
+        self.device = client(host=host, port = port, socket_io_timeout = timeout)
         self.device.open()
         self.T_states = self.device.temperature.state_code_dict()
         self._T_approach = self.device.temperature.approach_mode(1)
@@ -228,9 +344,6 @@ class opticool():
     
 def main():
     device = opticool()
-    print(device.T())
-    import time
-    device.stop()
     
 if __name__ == '__main__':
     main()
