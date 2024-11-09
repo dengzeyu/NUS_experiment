d07095150edb06a26539e1854064e613401cbaf6
roomtemp <roomtemp@RoomTemp.stf.nus.edu.sg>
2024-11-09 21:13:13 +0800

    Stable version, setget_delay_factor added

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ ratio_sweep3 = 1
 back_ratio_sweep3 = 1
 delay_factor3 = 1
 back_delay_factor3 = 1
-setget_time_delay = 10
+setget_delay_factor = 1
 stepper_flag = False
 #fastmode_slave_flag = False
 #fastmode_master_flag = False
@@ -2514,7 +2514,7 @@ class Sweeper1d(tk.Frame):
             self.preset.loc[0, 'filename_sweep'] = current_filename
             self.preset.to_csv(globals()['sweeper1d_path'], index = False)
         
-        #self.entry_from.after(1000, self.rewrite_preset)
+        self.entry_from.after(1000, self.rewrite_preset)
 
     def update_sweep_configuration(self):
         global from_sweep1
@@ -2632,7 +2632,7 @@ class Sweeper1d(tk.Frame):
         if self.status_back_and_forth_master.get() == 0:
             back_and_forth_master = 1
         else:
-            back_and_forth_master = self.back_and_forth_master_count
+            back_and_forth_master = self.back_and_forth_master_coun
             
         self.preset.loc[0, 'status_back_and_forth1'] = self.status_back_and_forth_master.get()
         self.preset.to_csv(globals()['sweeper1d_path'], index = False)
@@ -3826,7 +3826,7 @@ class Sweeper2d(tk.Frame):
         self.entry_from1 = tk.Entry(self)
         self.entry_from1.insert(0, self.from1_init)
         self.entry_from1.place(relx=0.17, rely=0.29)
-        #self.entry_from1.after(1000, self.rewrite_preset)
+        self.entry_from1.after(1000, self.rewrite_preset)
 
         self.entry_to1 = tk.Entry(self)
         self.entry_to1.insert(0, self.to1_init)
@@ -4453,7 +4453,7 @@ class Sweeper2d(tk.Frame):
         self.preset.loc[0, 'uniform'] = self.uniform
         self.preset.to_csv(globals()['sweeper2d_path'], index = False)
         
-        #self.entry_from1.after(1000, self.rewrite_preset)
+        self.entry_from1.after(1000, self.rewrite_preset)
 
     def update_sweep_configuration(self):
         global from_sweep1
@@ -6409,7 +6409,7 @@ class Sweeper3d(tk.Frame):
         self.entry_from1 = tk.Entry(self)
         self.entry_from1.insert(0, self.from1_init)
         self.entry_from1.place(relx=0.17, rely=0.29)
-        #self.entry_from1.after(1000, self.rewrite_preset)
+        self.entry_from1.after(1000, self.rewrite_preset)
 
         self.entry_to1 = tk.Entry(self)
         self.entry_to1.insert(0, self.to1_init)
@@ -7169,7 +7169,7 @@ class Sweeper3d(tk.Frame):
         self.preset.loc[0, 'uniform'] = self.uniform
         self.preset.to_csv(globals()['sweeper3d_path'], index = False)
         
-        #self.entry_from1.after(1000, self.rewrite_preset)
+        self.entry_from1.after(1000, self.rewrite_preset)
             
     def update_sweep_configuration(self):
         global from_sweep1
@@ -9015,11 +9015,11 @@ class Sweeper_write(threading.Thread):
         global back_and_forth_master
         global zero_time
         global script
+        global setget_delay_factor
         
         
         def setget_write():
             global setget_flag
-            global setget_time_delay
             global filename_setget
             global pause_flag
             global deli
@@ -9042,7 +9042,7 @@ class Sweeper_write(threading.Thread):
                             dataframe.append(None)
                             return
                             
-                    time.sleep(setget_time_delay)
+                    time.sleep(setget_delay_factor)
                         
                     with open(filename_setget, 'a', newline = '') as f_object:
                         try:
@@ -9056,7 +9056,7 @@ class Sweeper_write(threading.Thread):
                         finally:
                             f_object.close()
                 else:
-                    time.sleep(setget_time_delay)
+                    time.sleep(setget_delay_factor)
         
         def append_read_parameters():
             '''appends dataframe with parameters to read'''
