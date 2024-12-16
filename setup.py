d8c41ba6b0bfa25b23dbb2a04ea1f17d33c9cf93
unknown <graphene@DESKTOP10.stf.nus.edu.sg>
2024-12-16 15:32:47 +0800

    setup file added

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+   name='Unisweep',
+   version='1.0',
+   description='Automation software for acquiring and monitoring data in a routine way.',
+   author='Mikhail Kravtsov',
+   author_email='mishanya319@gmail.com',
+   packages=['devices', 'config', 'mapper', 'addons'],  #same as name
+   install_requires=['os', 'sys', 'json', 'csv', 'threading', 'tkinter', 'matplotlib', 'pyvisa-py'
+                     'time', 'datetime', 'pandas', 'numpy', 'scipy', 'glob', 'serial', 'IPy', 'pillow',
+                     'itertools', 'pymeasure', 
+                     'git+ssh://git@github.com/MSLNZ/msl-equipment/releases/download/v0.1.0/msl_equipment-0.1.0-py2.py3-none-any.whl#egg=msl-equipment'], 
+)
