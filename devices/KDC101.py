5318618f2e970c0ec7008727d882ec8055f1a2ec
Zeyu Deng (Jerry) <dengzeyu@gmail.com>
2025-01-31 12:02:23 +0800

    update

--- devices/KDC101.py
+++ devices/KDC101.py
@@ -17,12 +17,14 @@ class KDC101():
     def __init__(self, adress = ''):
         
         # ensure that the Kinesis folder is available on PATH
-        os.environ['PATH'] += os.pathsep + 'C:/Program Files/Thorlabs/Kinesis'
+        os.environ['PATH'] += os.pathsep + 'C:/Program Files (x86)/Thorlabs/Kinesis'
 
-        if adress == 'COM10':
-            ser = '27258071'
+        if adress == 'COM5':
+            ser = '27004046'
         elif adress == 'COM11':
-            ser = '27258084'
+            ser = '27004883'
+        elif adress == 'COM3':
+            ser = '27004770'
             
         record = EquipmentRecord(
             manufacturer='Thorlabs',
